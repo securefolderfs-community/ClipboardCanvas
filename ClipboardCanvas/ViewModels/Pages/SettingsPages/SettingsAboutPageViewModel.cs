c9aa07ae952e00c2e8099b297f2eea579dc16f7b
d2dyno006 <53011783+d2dyno006@users.noreply.github.com>
2021-05-13 12:37:06 +0200

    Improved About page

--- ClipboardCanvas/ViewModels/Pages/SettingsPages/SettingsAboutPageViewModel.cs
+++ ClipboardCanvas/ViewModels/Pages/SettingsPages/SettingsAboutPageViewModel.cs
@@ -1,13 +1,52 @@
 ﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using System.Windows.Input;
 
 namespace ClipboardCanvas.ViewModels.Pages.SettingsPages
 {
     public class SettingsAboutPageViewModel : ObservableObject
     {
+        #region Public Properties
+
         public string VersionNumber
         {
-            get => "1";
+            get => "1.0.0.0";
             //get => $"{Package.Current.Id.Version.Major}.{Package.Current.Id.Version.Minor}.{Package.Current.Id.Version.Build}.{Package.Current.Id.Version.Revision}";
         }
+
+        #endregion
+
+        #region Commands
+
+        public ICommand ShowChangelogCommand { get; private set; }
+
+        public ICommand SubmitFeedbackCommand { get; private set; }
+
+        #endregion
+
+        #region Constructor
+
+        public SettingsAboutPageViewModel()
+        {
+            // Create commands
+            ShowChangelogCommand = new RelayCommand(ShowChangelog);
+            SubmitFeedbackCommand = new RelayCommand(SubmitFeedback);
+        }
+
+        #endregion
+
+        #region Command Implementation
+
+        private void ShowChangelog()
+        {
+
+        }
+
+        private void SubmitFeedback()
+        {
+
+        }
+
+        #endregion
     }
 }
