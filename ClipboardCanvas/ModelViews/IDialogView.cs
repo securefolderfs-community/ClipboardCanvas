f08b163ecb8236a35cceafd1dcb35794f5fdf4c8
d2dyno006 <53011783+d2dyno006@users.noreply.github.com>
2021-12-08 11:38:57 +0100

    Fixed dialog regression

---
+++ ClipboardCanvas/ModelViews/IDialogView.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.UI.Xaml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClipboardCanvas.ModelViews
+{
+    public interface IDialogView
+    {
+        XamlRoot XamlRoot { get; set; }
+    }
+}
