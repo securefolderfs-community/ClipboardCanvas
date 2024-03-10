aca1ae84fdc2ffa25d8494f442e6603c290e4dc9
d2dyno006 <53011783+d2dyno006@users.noreply.github.com>
2024-03-10 16:41:58 +0100

    Hook up navigation

---
+++ src/ClipboardCanvas.WinUI/TemplateSelectors/CanvasTemplateSelector.cs
@@ -0,0 +1,20 @@
+﻿using ClipboardCanvas.Sdk.ViewModels.Controls.Canvases;
+using Microsoft.UI.Xaml;
+
+namespace ClipboardCanvas.WinUI.TemplateSelectors
+{
+    internal sealed class CanvasTemplateSelector : BaseTemplateSelector<BaseCanvasViewModel>
+    {
+        public DataTemplate? TextCanvasTemplate { get; set; }
+
+        /// <inheritdoc/>
+        protected override DataTemplate? SelectTemplateCore(BaseCanvasViewModel? item, DependencyObject container)
+        {
+            return item switch
+            {
+                TextCanvasViewModel => TextCanvasTemplate,
+                _ => base.SelectTemplateCore(item, container)
+            };
+        }
+    }
+}
