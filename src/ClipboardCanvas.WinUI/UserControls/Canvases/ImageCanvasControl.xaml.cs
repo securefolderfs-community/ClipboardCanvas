3b2f9bd73797b06f24fddbc1ef7fe0a5097da28b
d2dyno006 <53011783+d2dyno006@users.noreply.github.com>
2024-05-10 12:04:06 +0200

    Added image zoom

--- src/ClipboardCanvas.WinUI/UserControls/Canvases/ImageCanvasControl.xaml.cs
+++ src/ClipboardCanvas.WinUI/UserControls/Canvases/ImageCanvasControl.xaml.cs
@@ -1,6 +1,8 @@
 using ClipboardCanvas.Shared.ComponentModel;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Input;
+using Windows.Foundation;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -9,10 +11,50 @@ namespace ClipboardCanvas.WinUI.UserControls.Canvases
 {
     public sealed partial class ImageCanvasControl : UserControl
     {
+        private Point _lastPoint;
+        private bool _isDragging;
+
         public ImageCanvasControl()
         {
             InitializeComponent();
             IsEditing = false;
+        }
+
+        private void Scroller_PointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            Scroller.CapturePointer(e.Pointer);
+            _isDragging = IsZoomingEnabled;
+            _lastPoint = e.GetCurrentPoint((UIElement)sender).Position;
+        }
+
+        private void Image_PointerReleased(object sender, PointerRoutedEventArgs e)
+        {
+            _isDragging = false;
+        }
+
+        private void Scroller_PointerCaptureLost(object sender, PointerRoutedEventArgs e)
+        {
+            _isDragging = false;
+        }
+
+        private void Scroller_PointerMoved(object sender, PointerRoutedEventArgs e)
+        {
+            if (!IsZoomingEnabled)
+                return;
+
+            if (!_isDragging)
+                return;
+
+            var currentPoint = e.GetCurrentPoint((UIElement)sender).Position;
+            var dx = (currentPoint.X - _lastPoint.X) * 3.0f;
+            var dy = (currentPoint.Y - _lastPoint.Y) * 3.0f;
+
+            Scroller.ChangeView(
+                Scroller.HorizontalOffset - dx,
+                Scroller.VerticalOffset - dy,
+                null);
+
+            _lastPoint = currentPoint;
         }
 
         public IImage? Image
@@ -38,5 +80,19 @@ namespace ClipboardCanvas.WinUI.UserControls.Canvases
         }
         public static readonly DependencyProperty WasAlteredProperty =
             DependencyProperty.Register(nameof(WasAltered), typeof(bool), typeof(ImageCanvasControl), new PropertyMetadata(false));
+
+        public bool IsZoomingEnabled
+        {
+            get => (bool)GetValue(IsZoomingEnabledProperty);
+            set => SetValue(IsZoomingEnabledProperty, value);
+        }
+        public static readonly DependencyProperty IsZoomingEnabledProperty =
+            DependencyProperty.Register(nameof(IsZoomingEnabled), typeof(bool), typeof(ImageCanvasControl), new PropertyMetadata(false, (s, e) =>
+            {
+                if (s is not ImageCanvasControl control)
+                    return;
+
+                control.Scroller.ZoomMode = (bool)e.NewValue ? ZoomMode.Enabled : ZoomMode.Disabled;
+            }));
     }
 }
